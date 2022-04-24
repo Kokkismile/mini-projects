0b2cd73e88f630a56db32b7991b9f2a4ab3df895
Trine Sandberg <trine.marie.sandberg@gmail.com>
2022-04-24 23:03:03 +0200

    + "0" if hour/min/sec  < 10

--- js/clock.js
+++ js/clock.js
@@ -5,8 +5,11 @@
     let time = new Date();
 
       let hour = time.getHours();
+      hour = (hour < 10) ? "0" + hour : hour;
       let min = time.getMinutes();
+      min = (min < 10) ? "0" + min : min;
       let sec = time.getSeconds();
+      sec = (sec < 10) ? "0" + sec : sec;
 
     let displayTimeHtml = `<div class="clock-flex-wrap">
                       <div><p>${hour}</p></div>
