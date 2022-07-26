00ca91c0f16015cf0dbb926c94592f06778972e8
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-07-26 18:44:32 +0200

    div

--- js/calc.js
+++ js/calc.js
@@ -1,2 +1,5 @@
 const calcScreen = document.querySelector(".calc-screen");
 calcScreen.innerHTML = `<p>TEST</p>`
+
+const numberButtons = document.querySelectorAll("[data-number]");
+console.log(numberButtons)
